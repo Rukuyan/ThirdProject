5be22468ccd63768a578d57aa01f89d501ea92cf
Rukuyan <51773360+Rukuyan@users.noreply.github.com>
2019-06-18 13:22:14 -0700

    Update TuesdayJava.java

--- src/Tuesday/TuesdayJava.java
+++ src/Tuesday/TuesdayJava.java
@@ -3,6 +3,6 @@ package Tuesday;
 public class TuesdayJava {
     public static void main(String[] args){
         System.out.println("today is Tuesday");
-        System.out.println("today is Tuesday");
+        System.out.println("today is wednesday");
     }
 }
